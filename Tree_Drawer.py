c6abc438a438f5d88d307a6bf0cd5c64cdf85987
John Cartpenter <johncarpenter2008@gmail,com>
2025-02-12 10:55:36 -0600

    Keep Track of Angle

--- Tree_Drawer.py
+++ Tree_Drawer.py
@@ -4,10 +4,12 @@ splitCount = int(input("How many splits per branch?"))
 branchAngle = int(input("What's the angle between branches?"))
 treeHeight = int(input("How tall is the tree?"))
 trunkRatio = float(input("What's the ratio between Branches?"))
+randomness = int(input("How much Randomness?"))
 intialBranchLength = 100
 speed = 0
-
 intialAngle = (splitCount - 1) / 2 * branchAngle
+
+turtleAngle = 0
 
 screen = t.Screen()
 t.teleport(0,-300)
@@ -15,21 +17,26 @@ t.left(90)
 t.forward(intialBranchLength)
 t.speed(speed)
 
+def rotateTurtle(angle):
+	global turtleAngle
+	t.right(angle)
+	turtleAngle += angle
 
 def drawTree(RemainingHeight, branchLength):
 	if RemainingHeight == 0:
 		return
-	t.left(intialAngle)
+	rotateTurtle(-intialAngle)
 	for i in range(splitCount):
 		t.forward(branchLength)
 		drawTree(RemainingHeight - 1, branchLength * trunkRatio)
 		t.backward(branchLength)
-		t.right(branchAngle)
-	t.left(branchAngle)
-	t.left(intialAngle)
+		rotateTurtle(branchAngle)
+	rotateTurtle(-branchAngle)
+	rotateTurtle(-intialAngle)
 
 
 drawTree(treeHeight - 1, trunkRatio * intialBranchLength)
+t.backward(intialBranchLength)
 
 screen.exitonclick()
 
